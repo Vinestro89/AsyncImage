c011e428426203fd6060c8c220010c35b5e22451
Vincent DELMAESTRO <vincent.delmaestro@yahoo.com>
2021-09-09 18:52:56 +0200

    Initial Commit

---
+++ SwiftUI-Tutorial/SwiftUI_TutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialApp.swift
+//  SwiftUI-Tutorial
+//
+//  Created by Vincent Delmaestro on 08/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
